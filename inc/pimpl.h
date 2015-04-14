871d9ef73529f27df9175c4197b0eb6db50c415c
soda <mireaulf+github@gmail.com>
2015-04-13 22:22:20 -0400

    chore(pimpl)
    base of pimpl

---
+++ inc/pimpl.h
@@ -0,0 +1,18 @@
+#ifndef PIMPL_H_H
+#define PIMPL_H_H
+ 
+#include <memory>
+ 
+template<typename T>
+class pimpl {
+private:
+    std::unique_ptr<T> m;
+public:
+    pimpl();
+    template<typename ...Args> pimpl( Args&& ... );
+    ~pimpl();
+    T* operator->();
+    T& operator*();
+};
+ 
+#endif
